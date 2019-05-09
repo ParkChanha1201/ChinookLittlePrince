fb5a019a6127a62225be16c55e12179884ae368a
ltswer <ltswer@gmail.com>
2019-05-09 17:43:45 +0900

    Sync, test Form3

--- Chinook.UI/Form3.cs
+++ Chinook.UI/Form3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Chinook.Data;
 
 namespace Chinook.UI
 {
@@ -20,6 +21,9 @@ namespace Chinook.UI
         private void BtnResult_Click(object sender, EventArgs e)
         {
             //todo: Artist의 ArtistId를 검색해 해당 가수가 가장 많이 소유하고 있는 곡의 장르를 표시한다.
+            if (DataRepository.Album != null)
+                txtResult.Text = DataRepository.Album.GetByPK(int.Parse(txtInput.Text)).Title;
+
         }
     }
 }
