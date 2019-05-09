9d92db6ff82021659cf6f33586c6af7ae9d58931
chanha <chanhacraft@google.com>
2019-05-09 16:47:16 +0900

    .

---
+++ Chinook.Data/DataRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data
+{
+    class DataRepository
+    {
+        protected static ChinookEntities CreateContext()
+        {
+            ChinookEntities context = new ChinookEntities();
+            context.Database.Log = PrintToConsoleWindow;
+            return context;
+        }
+
+        private static void PrintToConsoleWindow(string log)
+        {
+            Console.WriteLine(log);
+        }
+
+        static DataRepository()
+        {
+            Playlist = new PlaylistData();
+        }
+
+        private static AlbumData Album { get; }
+
+        public static PlaylistData Playlist { get; }
+    }
+}
